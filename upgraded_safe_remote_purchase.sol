e3e0a183204fa329dad7ab9cb4683e46e7aca7e3
Bernardo Reis <92443646+bereis01@users.noreply.github.com>
2025-01-26 10:49:42 -0300

    Update upgraded_safe_remote_purchase.sol

--- upgraded_safe_remote_purchase.sol
+++ upgraded_safe_remote_purchase.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
+
 contract Purchase {
-    uint public value;
+    uint256 public value;
     address payable public seller;
     address payable public buyer;
 
     uint256 public lockTimeStamp;
 
-    enum State { Created, Ready, Locked, Released, Inactive }
+    enum State {
+        Created,
+        Ready,
+        Locked,
+        Released,
+        Finished,
+        Inactive
+    }
 
     State public state; // The state variable has a default value of the first member, `State.created`
 
@@ -22,20 +30,17 @@ contract Purchase {
     error ValueNotEven(); // The provided value has to be even.
 
     modifier onlyBuyer() {
-        if (msg.sender != buyer)
-            revert OnlyBuyer();
+        if (msg.sender != buyer) revert OnlyBuyer();
         _;
     }
 
     modifier onlySeller() {
-        if (msg.sender != seller)
-            revert OnlySeller();
+        if (msg.sender != seller) revert OnlySeller();
         _;
     }
 
     modifier inState(State state_) {
-        if (state != state_)
-            revert InvalidState();
+        if (state != state_) revert InvalidState();
         _;
     }
 
@@ -54,16 +59,15 @@ contract Purchase {
     // Ensure that `msg.value` is an even number.
     // Division will truncate if it is an odd number.
     // Check via multiplication that it wasn't an odd number.
-    function prepare()
+    function prepareContract()
         external
+        payable
         onlySeller
         inState(State.Created)
-        payable
     {
         // Conditions.
         value = msg.value / 2;
-        if ((2 * value) != msg.value)
-            revert ValueNotEven();
+        if ((2 * value) != msg.value) revert ValueNotEven();
 
         // State Change.
         state = State.Ready;
@@ -75,56 +79,36 @@ contract Purchase {
     // is called.
     function confirmPurchase()
         external
+        payable
         inState(State.Created)
         condition(msg.value == (2 * value))
-        payable
     {
         // State Change.
         emit PurchaseConfirmed();
         buyer = payable(msg.sender);
-        lockTimeStamp = block.timestamp + 1 days;
+        lockTimeStamp = block.timestamp + 7 days;
         state = State.Locked;
     }
 
     // Confirm that you (the buyer) received the item.
     // This will release the locked ether.
-    function confirmReceived()
-        external
-        onlyBuyer
-        inState(State.Locked)
-    {
+    function confirmReceived() external onlyBuyer {
+        // Conditions.
+        if (!((state == State.Locked) || (state == State.Released)))
+            revert InvalidState();
+
         // State Change.
         emit ItemReceived();
-        state = State.Released;
+        state = State.Finished;
 
         // Actions.
         buyer.transfer(value);
     }
 
-    // Resets the contract and reclaims the ether.
-    // Can only be called by the seller before
-    // the contract is locked or after it has
-    // been unlocked.
-    function reset()
-        external
-        onlySeller
-    {
-        // Conditions.
-        if (!((state == State.Created) || (state == State.Ready) || (state == State.Released)))
-            revert InvalidState();
-
-        // State Change.
-        emit SellerReset();
-        state = State.Created;
-
-        // Actions.
-        seller.transfer(address(this).balance);
-    }
-
-    // Releases funds if customer does not 
+    // Releases escrow if customer does not
     // confirm goods' receipt.
-    function release()
-        external 
+    function releaseEscrow()
+        external
         onlySeller
         inState(State.Locked)
         condition(block.timestamp > lockTimeStamp)
@@ -134,15 +118,31 @@ contract Purchase {
         state = State.Released;
 
         // Actions.
-        buyer.transfer(value);
+        seller.transfer(2 * value);
+    }
+
+    // Resets the contract and reclaims the ether.
+    // Can only be called by the seller before
+    // the contract is locked or after it has
+    // been unlocked.
+    function resetContract() external onlySeller {
+        // Conditions.
+        if (
+            !((state == State.Created) ||
+                (state == State.Ready) ||
+                (state == State.Finished))
+        ) revert InvalidState();
+
+        // State Change.
+        emit SellerReset();
+        state = State.Created;
+
+        // Actions.
+        seller.transfer(address(this).balance);
     }
 
     // Makes the contract inactive.
-    function abort()
-        external 
-        onlySeller
-        inState(State.Created)
-    {
+    function abortContract() external onlySeller inState(State.Created) {
         // State Change.
         emit Aborted();
         state = State.Inactive;
